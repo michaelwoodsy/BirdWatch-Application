22248676095d915916712aac1d219332c2dff531
Henry Barrett <hrb74@uclive.ac.nz>
2022-10-03 20:24:55 +1300

    Hide navigation bar on splash screen using Animated transition.

--- app/src/main/java/nz/ac/uclive/ojc31/seng440assignment2/graphs/NavGraph.kt
+++ app/src/main/java/nz/ac/uclive/ojc31/seng440assignment2/graphs/NavGraph.kt
@@ -1,10 +1,16 @@
 package nz.ac.uclive.ojc31.seng440assignment2.graphs
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -18,12 +24,21 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun NavGraph(navController: NavHostController) {
+    val showNavigationBar = rememberSaveable { (mutableStateOf(true))}
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    // hide navigation bar on these pages
+    when (navBackStackEntry?.destination?.route) {
+        Screen.Splash.route -> showNavigationBar.value = false
+        else -> showNavigationBar.value = true
+    }
 
     Scaffold(
         bottomBar = {
             NavigationBar(
                 navController = navController,
-                items = listOf(Screen.Home) // Add more screens here
+                items = listOf(Screen.Home), // Add more screens here
+                showNavigationBar = showNavigationBar
             )
         }
     ) { innerPadding ->
@@ -46,24 +61,30 @@ fun NavGraph(navController: NavHostController) {
 
 
 @Composable
-fun NavigationBar(navController: NavHostController, items: List<Screen>) {
-    BottomNavigation {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentDestination = navBackStackEntry?.destination
+fun NavigationBar(navController: NavHostController, items: List<Screen>, showNavigationBar: MutableState<Boolean>) {
+    AnimatedVisibility(
+        visible = showNavigationBar.value,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = { it }),
+    ) {
+        BottomNavigation {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
 
-        items.forEach { screen ->
-            BottomNavigationItem(
-                selected = currentDestination?.hierarchy?.any {it.route == screen.route} == true,
-                label = {Text(stringResource(screen.description))},
-                icon = { Icon(screen.icon, "" ) },
-                onClick = {
-                    navController.navigate(screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+            items.forEach { screen ->
+                BottomNavigationItem(
+                    selected = currentDestination?.hierarchy?.any {it.route == screen.route} == true,
+                    label = {Text(stringResource(screen.description))},
+                    icon = { Icon(screen.icon, stringResource(screen.description) ) },
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
                         }
-                        launchSingleTop = true
-                    }
-                })
+                    })
+            }
         }
     }
 }
