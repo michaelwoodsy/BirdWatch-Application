997a28f1824bff2072ad65ac1e665316e7d92af5
Michael <mdw85@uclive.ac.nz>
2022-10-06 19:00:16 +1300

    Maps now working as intended (finally lol).

--- app/src/main/java/nz/ac/uclive/ojc31/seng440assignment2/screens/MapScreen.kt
+++ app/src/main/java/nz/ac/uclive/ojc31/seng440assignment2/screens/MapScreen.kt
@@ -1,5 +1,7 @@
 package nz.ac.uclive.ojc31.seng440assignment2.screens
 
+import android.annotation.SuppressLint
+import android.location.Location
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,10 +15,12 @@ import androidx.compose.ui.unit.dp
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 
+@SuppressLint("MissingPermission")
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MapScreen() {
@@ -29,9 +33,11 @@ fun MapScreen() {
         android.Manifest.permission.ACCESS_COARSE_LOCATION
     )
 
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
     val universityOfCanterbury = LatLng(-43.5225, 172.5794)
     var cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(universityOfCanterbury, 10f)
+        position = CameraPosition.fromLatLngZoom(universityOfCanterbury, 15f)
     }
     val uiSettings = remember {
         MapUiSettings()
@@ -45,12 +51,24 @@ fun MapScreen() {
 
     when (fineLocationPermissionState.status) {
         is PermissionStatus.Granted -> {
-
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    if (location != null) {
+                        val currentPosition = LatLng(location.latitude, location.longitude)
+                        cameraPositionState.position = CameraPosition.fromLatLngZoom(currentPosition, 15f)
+                    }
+                }
         }
         is PermissionStatus.Denied -> {
             when (coarseLocationPermissionState.status) {
                 is PermissionStatus.Granted -> {
-
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location : Location? ->
+                            if (location != null) {
+                                val currentPosition = LatLng(location.latitude, location.longitude)
+                                cameraPositionState.position = CameraPosition.fromLatLngZoom(currentPosition, 15f)
+                            }
+                        }
                 }
                 is PermissionStatus.Denied -> {
                     properties = MapProperties(
