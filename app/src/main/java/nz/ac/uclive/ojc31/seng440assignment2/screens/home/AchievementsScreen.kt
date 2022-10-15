20a4d86484b3d1dd5c0213f5144be694c0cf5f89
Michael <mdw85@uclive.ac.nz>
2022-10-15 17:28:13 +1300

    Added empty challenges, achievements and statistics screens.

---
+++ app/src/main/java/nz/ac/uclive/ojc31/seng440assignment2/screens/home/AchievementsScreen.kt
@@ -0,0 +1,11 @@
+package nz.ac.uclive.ojc31.seng440assignment2.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun AchievementsScreen(
+    navController: NavHostController
+) {
+
+}
