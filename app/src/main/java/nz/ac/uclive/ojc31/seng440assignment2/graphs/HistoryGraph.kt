e3955c1939b71fd1e776f4ea893eac60e0c4cc34
Henry Barrett <hrb74@uclive.ac.nz>
2022-10-18 10:03:15 +1300

    Finally fixed the jumping UI issue on view entry screen by drawing a box behind the main content

--- app/src/main/java/nz/ac/uclive/ojc31/seng440assignment2/graphs/HistoryGraph.kt
+++ app/src/main/java/nz/ac/uclive/ojc31/seng440assignment2/graphs/HistoryGraph.kt
@@ -1,8 +1,11 @@
 package nz.ac.uclive.ojc31.seng440assignment2.graphs
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -22,12 +25,18 @@ fun NavGraphBuilder.historyGraph(navController: NavHostController, innerPadding:
     composable(route = SubScreen.ViewEntryScreen.route) { navBackStackEntry ->
         val backStackEntry =
             remember(navBackStackEntry) { navController.getBackStackEntry(Screen.History.route) }
-        Box(Modifier.padding(innerPadding)) {
-            SwipeToReturn(navController = navController) {
-                ViewEntryScreen(
-                    navController = navController,
-                    historyViewModel = hiltViewModel(backStackEntry)
-                )
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colors.primary)
+        ) {
+            Box(Modifier.padding(innerPadding)) {
+                SwipeToReturn(navController = navController) {
+                    ViewEntryScreen(
+                        navController = navController,
+                        historyViewModel = hiltViewModel(backStackEntry)
+                    )
+                }
             }
         }
     }
